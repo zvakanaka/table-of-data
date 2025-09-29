08ac07ab3398d48b8ee203e00bcde854f0bec5ca
Adam Quinton <8365885+zvakanaka@users.noreply.github.com>
2025-09-29 13:21:26 -0600

    html -> js

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 # Table Custom Element
 [demo](https://zvakanaka.github.io/table-of-data)
 ## About
-Lightweight and easy to use data table with some markdown support.
+Lightweight and easy to use data table
 ## Usage
 Import the element in the head
 ```html
-<link rel="import" href="table-of-data.html">
+<script src="./table-of-data.html">
 ```
 Put the element somewhere in the body
 ```html
@@ -17,10 +17,10 @@ let data = {
   headings: [ 'State', 'Place' ],
   rows: [
     [ 'California',
-      '[Redwood Forest](https://en.wikipedia.org/wiki/Redwood_National_and_State_Parks)'
+      'Redwood Forest'
     ],
     [ 'New York',
-      '[New York Islands](https://en.wikipedia.org/wiki/List_of_islands_of_New_York)'
+      'New York Islands'
     ]
   ]
 };
@@ -28,4 +28,4 @@ document.querySelector('table-of-data').data = data;
 ```
 ![Screenshot](http://i.imgur.com/Wi9b2Jy.png)
 ## Polyfill
-Only HTML Imports and Custom Elements need to be polyfilled (`webcomponents-hi-ce.js`). To see how to use a polyfill, check out [index.html](https://github.com/zvakanaka/table-of-data/blob/master/index.html#L29-L56), which only loads the polyfill if needed (credit to [Eric Bidelman](https://gist.github.com/ebidel/1d5ede1e35b6f426a2a7)).
+Only Custom Elements needs to be polyfilled (`webcomponents-hi-ce.js`). To see how to use a polyfill, check out [index.html](https://github.com/zvakanaka/table-of-data/blob/master/index.html#L29-L56), which only loads the polyfill if needed (credit to [Eric Bidelman](https://gist.github.com/ebidel/1d5ede1e35b6f426a2a7)).
---
+++ table-of-data.js
@@ -0,0 +1,44 @@
+(function() {
+  'use strict';
+  customElements.define('table-of-data', class extends HTMLElement {
+    constructor() {
+      super(); //always
+    }
+    connectedCallback() {//event listeners
+      this.innerHTML = `<table id="table" class="table-of-data"></table>
+        <style>
+          table.table-of-data td, table.table-of-data th { border-bottom: 1px solid #ddd; text-align: left; }
+          table.table-of-data { border-collapse: collapse; width: 100%; }
+          table.table-of-data th, table.table-of-data td { padding: 8px; }
+        </style>`;
+    }
+    disconnectedCallback() {}//TODO: event cleanup
+    set data(val) {
+      this._refreshTable(val);
+      this._data = val;
+    }
+    get data() { return (this._data || {}); }
+    _refreshTable(data) {//add/refresh data in table
+      let tableOfData = this.children.table;
+      let frag = document.createDocumentFragment();
+      let tableHeadingRow = document.createElement('tr');
+      data.headings.forEach(heading => {
+        let tableHeading = document.createElement('th');
+        tableHeading.textContent = heading;
+        tableHeadingRow.appendChild(tableHeading);
+      });
+      frag.appendChild(tableHeadingRow);
+      data.rows.forEach(row => {
+        let tableRow = document.createElement('tr');
+        data.headings.forEach((name, i) => {
+          let rowEl = document.createElement('td');
+          rowEl.innerHTML = row[i];
+          tableRow.appendChild(rowEl);
+        });
+        frag.appendChild(tableRow);
+      });
+      tableOfData.innerHTML = '';//clear the table
+      tableOfData.appendChild(frag);
+    }
+  });
+}());
